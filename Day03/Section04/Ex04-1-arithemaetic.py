467a34344c15fff6bc8081a39b0fa58baddbc07a
eve5874 <dasol5874@gmail.com>
2022-11-26 12:47:56 +0900

    dd

---
+++ Day03/Section04/Ex04-1-arithemaetic.py
@@ -0,0 +1,17 @@
+a = 7
+b = 2
+
+#덧셈
+print('{}+{} = {}'.format(a,b,a+b))
+#뺄셈
+print('{}-{} = {}'.format(a,b,a-b))
+#곱셈
+print('{}*{} = {}'.format(a,b,a*b))
+#거듭제곱
+print('{}**{} = {}'.format(a,b,a**b))
+#나누기
+print('{}/{} = {}'.format(a,b,a/b))
+#몫
+print('{}//{} = {}'.format(a,b,a//b))
+#나머지
+print('{}%{} = {}'.format(a,b,a%b))
